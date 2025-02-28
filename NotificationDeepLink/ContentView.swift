70c4d5f9886821cbd6b0a6bd00ebb58a8d539a03
harsh <bighugscafe@gmail.com>
2025-02-28 18:34:05 +0530

    Initial commit

--- NotificationDeepLink/ContentView.swift
+++ NotificationDeepLink/ContentView.swift
@@ -8,15 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+	@Environment(AppData.self) private var appData
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+		// Bindable Envrionment Values
+		@Bindable var appData = appData
+		NavigationStack(path: $appData.mainPageNavigationPath) {
+			List {
+				NavigationLink("View 1", value: "View1")
+				NavigationLink("View 2", value: "View2")
+				NavigationLink("View 3", value: "View3")
+				}
+				.navigationTitle(Text("Notification Deep Link"))
+				// Navigation Destination Views
+				.navigationDestination(for: String.self) { value in
+					Text("Hello From \(value)")
+						.navigationTitle(value)
+				}
+			}
+			.task {
+					/// Notification Permissions
+				let _ = try? await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound])
+			}
+		}
 }
 
 #Preview {
--- NotificationDeepLink/NotificationDeepLinkApp.swift
+++ NotificationDeepLink/NotificationDeepLinkApp.swift
@@ -9,9 +9,52 @@ import SwiftUI
 
 @main
 struct NotificationDeepLinkApp: App {
+	@UIApplicationDelegateAdaptor(AppData.self) private var appData
     var body: some Scene {
         WindowGroup {
             ContentView()
+				.environment(appData)
         }
     }
+}
+
+@Observable
+class AppData : NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+	var mainPageNavigationPath: [String] = []
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		UNUserNotificationCenter.current().delegate = self
+		return true
+	}
+	
+	//willpresent
+	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+		// Showing alert even when app is active
+		
+		return [.sound, .banner]
+	}
+	
+	// Handling Notification
+	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+		//print(response.notification.request.content.userInfo)
+		
+//		[AnyHashable("Simulator Target Bundle"): com.AnimeAI.NotificationDeepLink, AnyHashable("aps"): {
+//			alert =     {
+//				body = "Go to View 1";
+//				title = "Push Notification's";
+//			};
+//			badge = 0;
+//		}, AnyHashable("pageLink"): View1]
+		
+		if let pageLink = response.notification.request.content.userInfo["pageLink"] as? String {
+			if mainPageNavigationPath.last != pageLink {
+				// Optional: Removing all previous pages
+				// You can skip this step if you want to maintain previous pages as well
+				mainPageNavigationPath = []
+				// Pushing our new page
+				mainPageNavigationPath.append(pageLink)
+			}
+		}
+		
+	}
 }

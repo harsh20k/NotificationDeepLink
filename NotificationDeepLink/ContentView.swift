9e030dd372b757dd525c2c5444a0ca70efb3230f
harsh <bighugscafe@gmail.com>
2025-02-28 17:35:31 +0530

    Initial Commit

---
+++ NotificationDeepLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationDeepLink
+//
+//  Created by harsh  on 28/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationDeepLink/NotificationDeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationDeepLinkApp.swift
+//  NotificationDeepLink
+//
+//  Created by harsh  on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationDeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
